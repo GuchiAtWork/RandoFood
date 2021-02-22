0089659ed2017c72daf4c7572fe0d5387caae79b
Keizo Hamaguchi <guchi@pop-os.localdomain>
2021-02-22 17:41:34 +0900

    mobile app skeleton

---
+++ app/src/main/java/com/example/randofood/MainActivity.kt
@@ -0,0 +1,11 @@
+gin package com.example.randofood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
