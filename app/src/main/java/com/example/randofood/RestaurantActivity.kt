8b24d174ec71812aebc2b37a745f46a6cd6bbdf2
Keizo Hamaguchi <guchi@pop-os.localdomain>
2021-02-25 23:32:39 +0900

    now show pictures of restaurants

--- app/src/main/java/com/example/randofood/RestaurantActivity.kt
+++ app/src/main/java/com/example/randofood/RestaurantActivity.kt
@@ -4,8 +4,22 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Klaxon
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.PhotoMetadata
+import com.google.android.libraries.places.api.net.FetchPhotoRequest
+import com.google.android.libraries.places.api.net.FetchPhotoResponse
+import java.io.StringReader
+import java.lang.Exception
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 import kotlin.random.Random
@@ -13,12 +27,14 @@ import kotlin.random.Random
 class RestaurantActivity : AppCompatActivity() {
 
     private var restaurants: ArrayList<HashMap<String, String?>> = ArrayList()
-    private var random: Random = Random(10)
+    private val random: Random = Random(10)
     private var chosenRestaurant: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant)
+
+        Places.initialize(this, BuildConfig.GOOGLE_KEY)
 
         restaurants = intent.getSerializableExtra("restaurants") as ArrayList<HashMap<String, String?>>
         renderRestaurant(restaurants)
@@ -71,6 +87,34 @@ class RestaurantActivity : AppCompatActivity() {
                 text = "Is It Open? NO!"
             }
         }
+
+        if (restaurant["photo_reference"] != null) {
+            getRestaurantPicture(restaurant)
+        } else {
+            val imageView = findViewById<ImageView>(R.id.imageView)
+            imageView.setImageResource(R.mipmap.ic_launcher_foreground)
+        }
+    }
+
+    private fun getRestaurantPicture(restaurant: HashMap<String, String?>) {
+        val placesClient = Places.createClient(this)
+
+        val photoData = PhotoMetadata.builder(restaurant["photo_reference"] ?: "").build()
+
+        val photoRequest = FetchPhotoRequest.builder(photoData)
+                .setMaxWidth(291)
+                .setMaxHeight(227)
+                .build()
+        placesClient.fetchPhoto(photoRequest)
+                .addOnSuccessListener { fetchPhotoResponse: FetchPhotoResponse ->
+                    val bitmap = fetchPhotoResponse.bitmap
+                    val imageView = findViewById<ImageView>(R.id.imageView)
+                    imageView.setImageBitmap(bitmap)
+                }.addOnFailureListener { exception: Exception ->
+                    println(exception)
+                    val imageView = findViewById<ImageView>(R.id.imageView)
+                    imageView.setImageResource(R.mipmap.ic_launcher_foreground)
+                }
     }
 
     private fun determineRestaurant(size: Int): Int {
